fe2965a46d704e02fbfe6f80546c91cb48a93565
MaxioN <maxion03@gmail.com>
2018-03-17 17:12:41 +0300

    ChatListLayout done

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -11,16 +11,13 @@ class App extends Component {
         <header className="app-header">
           <h1>Header</h1>
         </header>
-<<<<<<< HEAD
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
           <ListItem name="lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem " lastMessage="lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem " newMessages="3" date="1 day ago"/>
-=======
         <div className="app-content">
         </div>
         <SendMessage />
->>>>>>> Создал плашку для ввода сообщений
       </div>
     );
   }
