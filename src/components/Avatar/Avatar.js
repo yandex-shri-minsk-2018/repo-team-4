fcd50d196a954c0d5a1bcf2c9a27c512be22e546
vladpro3 <Bondarovich77@gmail.com>
2018-03-17 13:54:49 +0300

    'Avatar'

---
+++ src/components/Avatar/Avatar.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import avatar_img from './default-avatar.png';
+import './Avatar.css';
+
+class Avatar extends Component{
+    constructor(props) {
+        super(props);
+
+        if (this.props.size === "small")
+            this.classAvatarSize = 'avatar__img__chat';
+        else this.classAvatarSize = 'avatar__img__profile';
+
+        if (this.props.url)
+            this.image = this.props.url;
+        else this.image = avatar_img;
+    }
+
+    render(){
+        return(
+            <div className={this.classAvatarSize}>
+                <img src={this.image} className='avatar__img' alt="avatar" />
+            </div>
+        );
+    }
+}
+
+export default Avatar;
