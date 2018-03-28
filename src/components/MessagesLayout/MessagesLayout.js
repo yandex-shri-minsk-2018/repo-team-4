9476d51e7c6459d8d88cc1f461c56c98358077ad
vladpro3 <Bondarovich77@gmail.com>
2018-03-28 22:35:06 +0300

    Приблизительно адаптировал для подключения к бд. Фикс балуна

--- src/components/MessagesLayout/MessagesLayout.js
+++ src/components/MessagesLayout/MessagesLayout.js
@@ -6,60 +6,70 @@ import SendMessage from '../SendMessage/SendMessage'
 
 import './MessagesLayout.css';
 
-class MessagesLayout extends Component {
-    constructor(props) {
-        super(props);
+export class MessagesLayout extends Component {
+  constructor(props) {
+    super(props);
 
-        this.incomingMessageAvatar = 'https://dcnt5qvi2hv76.cloudfront.net/b833369/resize_cache/74316/2e7fb5fb2ab1ebdd663145ea3b6c2c2e/main/e51/e51a3c0243a0c3463d729bea7c5b18b7/photo.jpg?h=ncaby.bitrix24.by';
-        this.myAvatar = 'https://vignette.wikia.nocookie.net/borderlands/images/1/13/Awesome.png/revision/latest?cb=20091026223409';
-        this.incomingMessage = 'Hi Andrea! How are you?';
-        this.outgoingMessage = 'Doing good, how do you feel about grabbing a coffee sometime?';
+    this.incomingMessageAvatar = 'https://dcnt5qvi2hv76.cloudfront.net/b833369/resize_cache/74316/2e7fb5fb2ab1ebdd663145ea3b6c2c2e/main/e51/e51a3c0243a0c3463d729bea7c5b18b7/photo.jpg?h=ncaby.bitrix24.by';
+    this.myAvatar = 'https://vignette.wikia.nocookie.net/borderlands/images/1/13/Awesome.png/revision/latest?cb=20091026223409';
 
-        if (props.incomingMessage)
-            this.incomingMessage = props.incomingMessage;
-        if (props.outgoingMessage)
-            this.outgoingMessage = props.outgoingMessage;
+    /**
+     * todo: передавать в балун Timestamp сообщения в формате 'hh:mm', реализовать поддержку
+     * todo: получать аватар пользователя в зависимости от его id. Для этого добавить в сущность User поле 'Avatar'
+     * todo: асинхронность
+     *
+     * userId 1001 - авторизированный пользователь
+     * Пока что в псевдо-бд стоит рандом входящих/исходящих сообщений
+     */
 
-        /*
-         * Не забыть добавить key, после связки с БД,
-         * Сделать вывод сообщений в зависимости от времени их написания
-         * {
-         *      _id: string,       // Уникальный идентификатор сообшения
-         *      userId: string,    // Идентификатор пользователя, отправившего сообщение (User._id)
-         *      message: string,   // Текстовое сообщение
-         *      created_at: number // Timestamp времени отправки
-         * }
-         */
+    // let messages = await api.getRoomMessages(roomId);
+    this.dbMessage = [];
+    for (let i = 0; i < 10; i++)
+      this.dbMessage.push({
+          _id: "000" + i.toString(),
+          userId: "100" + Math.floor(1 + Math.random() * 2),
+          message: "BlaBlaBlaBlaBlaBlaBla BlaBlaBlaBlaBlaBlaBla__" + i.toString(),
+          created_at: 28032018171900 + i
+        })
 
-        this.messages = [];
-        for (let i = 0; i < 5; i++) {
-            this.messages.push(
-                <Message url={this.incomingMessageAvatar} messageText={this.incomingMessage} isMyMessage={false}/>,
-                <Message url={this.myAvatar} messageText={this.outgoingMessage} isMyMessage={true}/>
-            )
-        }
+    /**
+     * todo: получать аватарку получив информацию о юзере. userId брать из сообщения
+     *
+     * userId = this.dbMessage[i]._id
+     * let user = await api.getUser(userId);
+     * url={user.avatar}
+     */
+
+    this.messages = [];
+    for (let i = 0; i < 10; i++) {
+      this.messages.push(
+        <Message
+          key={this.dbMessage[i]._id}
+          url={this.dbMessage[i].userId === "1001" ? this.myAvatar : this.incomingMessageAvatar}
+          messageText={this.dbMessage[i].message}
+          isMyMessage={this.dbMessage[i].userId === "1001"}/>
+      )
     }
+  }
 
-    componentDidMount() {
-        document.getElementById('messagesLayout__messageList')
-            .scrollTo(0, document.getElementById('messagesLayout__messageList').scrollHeight)
-    }
+  componentDidMount() {
+    document.getElementById('messagesLayout__messageList')
+      .scrollTo(0, document.getElementById('messagesLayout__messageList').scrollHeight)
+  }
 
-    render() {
-        return (
-            <div className='messagesLayout'>
-                <div className='messagesLayout__header'>
-                    <Header/>
-                </div>
-                <div className='messagesLayout__messageList' id='messagesLayout__messageList'>
-                    {this.messages.map(item => item)}
-                </div>
-                <div className='messagesLayout__textInput'>
-                </div>
-                <SendMessage/>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className='messagesLayout'>
+        <div className='messagesLayout__header'>
+          <Header/>
+        </div>
+        <div className='messagesLayout__messageList' id='messagesLayout__messageList'>
+          {this.messages.map(item => item)}
+        </div>
+        <div className='messagesLayout__textInput'>
+        </div>
+        <SendMessage/>
+      </div>
+    )
+  }
 }
-
-export default MessagesLayout;
