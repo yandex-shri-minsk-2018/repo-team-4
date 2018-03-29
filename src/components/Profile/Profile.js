7f6435091ed96de43fc22a9f4bc29cf180b15076
BadVal99 <ver.valery99@gmail.com>
2018-03-29 18:29:40 +0300

    addProfile size

---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import './Profile.css';
+import ProfileHeader from './ProfileHeader'
+import Avatar from '../Avatar/Avatar'
+
+
+
+class Profile extends Component {
+  render() {
+    var avatar = 'https://dcnt5qvi2hv76.cloudfront.net/b833369/resize_cache/74316/2e7fb5fb2ab1ebdd663145ea3b6c2c2e/main/e51/e51a3c0243a0c3463d729bea7c5b18b7/photo.jpg?h=ncaby.bitrix24.by';
+    return (
+      <div className='Profile__wrapper'>
+        <div className='wrapper-background'>
+          <ProfileHeader />
+          <div className='Profile__main'>
+            <div className='Profile__avatar'>
+              <Avatar url={avatar}/>
+            </div>
+            <div className='Profile__name'>
+              {this.props.name}
+            </div>
+            <div className='Profile__Edit'>
+              Редактировать
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Profile;
