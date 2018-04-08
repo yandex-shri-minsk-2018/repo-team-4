e28111ea437c92e659b514ccad51fe71fb3ab99c
Vladimir Zubritskii <Vovavipse@gmail.com>
2018-04-08 15:13:26 +0300

    new connect syntax

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,8 @@
-import { combineReducers } from 'redux'
-import navigation from './navigation/reducer'
-import chat from './chat/reducer'
+import {combineReducers} from "redux";
+import navigation from "./navigation/reducer";
+import chat from "./chat/reducer";
 
 export default combineReducers({
     navigation,
-    chat,
-})
+    chat
+});
